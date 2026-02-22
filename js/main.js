058989dc1bf787a8c2674d97421117626bce7bcb
berenicedevos <berenicedevos@outlook.com>
2026-02-22 20:24:07 -0300

    Mejora en funcionalidad

--- js/main.js
+++ js/main.js
@@ -1,191 +1,340 @@
-// ARRAY DE ESPECIALIDADES
-const especialidad = [
-  { id: 1, nombre: "Cardiología" },
-  { id: 2, nombre: "Neurología" },
-  { id: 3, nombre: "Endocrinología" },
-  { id: 4, nombre: "Traumatología" },
-  { id: 5, nombre: "Reumatología" },
-  { id: 6, nombre: "Dermatología" },
-  { id: 7, nombre: "Rehabilitación" },
-  { id: 8, nombre: "Obstetricia" },
-  { id: 9, nombre: "Clínica" },
-  { id: 10, nombre: "Fisiatría / Kinesio" },
-  { id: 11, nombre: "Fonoaudiología" },
-  { id: 12, nombre: "Psicología" }
-];
+let baseDatos = [];
+const URL = "./db/data.json";
 
-// ARRAY DE HORARIOS
-const horarios = [
-  "08:00",
-  "09:00",
-  "10:00",
-  "11:00",
-  "15:00",
-  "16:00",
-  "17:00"
-];
-
-// ARRAY PARA GUARDAR TURNO AGENDADOS
-const turnosAgendados = JSON.parse(localStorage.getItem("turnos")) || [];
-document.addEventListener("DOMContentLoaded", mostrarTurnos);
-
-//Borra turnos agendados
-//const turnosAgendados = [];   
-//localStorage.removeItem("turnos");
-//turnosAgendados.length = 0;  
-//listaTurnos.innerHTML = ""; 
-
-
-// CONEXION CON DOM
+// REFERENCIAS DOM
 const selectEspecialidad = document.getElementById("especialidad");
+const selectMedico = document.getElementById("medico");
+const selectFecha = document.getElementById("fecha");
 const selectHorario = document.getElementById("horario");
 const formTurno = document.getElementById("formTurno");
-const mensaje = document.getElementById("mensaje");
-const listaTurnos = document.getElementById("listaTurnos");
+
+const inputDni = document.getElementById("dni");
 const inputNombre = document.getElementById("nombre");
 const inputApellido = document.getElementById("apellido");
-const inputDni = document.getElementById("dni");
-
-// CARGA LOS VALORES DE LA LISTA DESPLEGABLE
-especialidad.forEach(e => {
-  const option = document.createElement("option");
-  option.value = e.nombre;
-  option.textContent = e.nombre;
-  selectEspecialidad.appendChild(option);
-});
-
-horarios.forEach(h => {
-  const option = document.createElement("option");
-  option.value = h;
-  option.textContent = h;
-  selectHorario.appendChild(option);
-});
-
-//FORMULARIO TURNO (EVENTO SUBMIT)
-formTurno.addEventListener("submit", function (evento) {
-  evento.preventDefault();
-
-  const especialidadSeleccionada = selectEspecialidad.value;
-  const horarioSeleccionado = selectHorario.value;
-  const nombre = inputNombre.value.trim().toUpperCase();
-  const apellido = inputApellido.value.trim().toUpperCase();
-  const dni = parseInt(inputDni.value.trim());
-
-  //Valido datos del paciente
-  const esValido = validarDatosPacientes(nombre, apellido, dni)
-  if (!esValido) return;
-
-  //Verifico si el turno esta ocupado
-  if (turnoOcupado(especialidadSeleccionada, horarioSeleccionado)) {
-    mostrarError("El horario ya se encuentra ocupado para la especialidad seleccionada");
-    return;
-  }
-
-  //Agendo el turno
-  const turno = {
-  especialidad: especialidadSeleccionada,
-  horario: horarioSeleccionado,
-  nombre: nombre,
-  apellido: apellido,
-  dni: dni
-  };
-
-  turnosAgendados.push(turno);
-  localStorage.setItem("turnos", JSON.stringify(turnosAgendados));
-
-  mostrarExito("El turno se ha agendado correctamente")
-  formTurno.reset();
-
-  //Actualizo la lista de turnos agendados
-  mostrarTurnos();
-});
+const inputFechaNacimiento = document.getElementById("fechaNacimiento");
+const inputEmail = document.getElementById("email");
+const inputTelefono = document.getElementById("telefono");
 
 
+// CONSTRUCTOR DE TURNOS
+class Turno {
 
-//----------------------------------------FUNCIONES--------------------------------------------
-
-// FUNCIÓN 1: Validar datos del paciente
-function validarDatosPacientes(nombrePaciente, apellidoPaciente, dniPaciente){
-
-    if (nombrePaciente === "" ||nombrePaciente === null || isNaN(nombrePaciente)===false){
-       mostrarError("El nombre ingresado es inválido")
-        return false
-    }  
-
-    if (apellidoPaciente === "" ||apellidoPaciente === null || !isNaN(apellidoPaciente)){
-       mostrarError("El apellido ingresado es inválido")
-      return false
+    constructor(nombre, apellido, dni, email, telefono, fechaNacimiento, especialidadId, especialidad, medicoId, medico, fecha, hora){
+        this.id = Date.now();
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+        this.especialidadId = especialidadId;
+        this.especialidad = especialidad;
+        this.medicoId = medicoId
+        this.medico = medico;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado = "Pendiente";
     }
-
-    if (dniPaciente === "" || dniPaciente===null || isNaN(dniPaciente)===true || dniPaciente<10000 || dniPaciente>1000000000){
-      mostrarError("El DNI ingresado es inválido")
-      return false
-    } 
-  return true
 }
 
-// FUNCIÓN 2: Verficar si el turno esta ocupado
-function turnoOcupado(especialidad, horario) {
-  return turnosAgendados.some(turno =>
-    turno.especialidad === especialidad &&
-    turno.horario === horario
-  );
-}
+let turnosAgendados = JSON.parse(localStorage.getItem("turnos")) || [];
 
-// FUNCIÓN 3: Mostrar mensaje de error al agendar turno
-function mostrarError(texto) {
-  mensaje.textContent = texto;
-  mensaje.className = "alert alert-danger";
-  mensaje.classList.remove("d-none");
-}
+function limpiarTurnosViejos() {
 
-// FUNCIÓN 4: Mostrar mensaje de exito al agendar turno
-function mostrarExito(texto) {
-  mensaje.textContent = texto;
-  mensaje.className = "alert alert-success";
-  mensaje.classList.remove("d-none");
-}
+    const hoy = new Date();
+    hoy.setHours(0,0,0,0);
 
-// FUNCIÓN 5: Mostrar turnos
-function mostrarTurnos() {
-  listaTurnos.innerHTML = "";
+    turnosAgendados = turnosAgendados.filter(turno => {
 
-  const turnosAgrupados = agruparTurnosPorEspecialidad(turnosAgendados);
+        const [anio, mes, dia] = turno.fecha.split("-");
+        const fechaTurno = new Date(anio, mes-1, dia);
 
-  for (const especialidad in turnosAgrupados) {
-    // Título de la especialidad
-    const titulo = document.createElement("li");
-    titulo.className = "list-group-item active";
-    titulo.textContent = especialidad;
-    listaTurnos.appendChild(titulo);
-
-  turnosAgrupados[especialidad].sort((a, b) => a.horario.localeCompare(b.horario));
-
-    // Turnos de esa especialidad
-    turnosAgrupados[especialidad].forEach(turno => {
-      const li = document.createElement("li");
-      li.className = "list-group-item";
-      li.textContent =
-        `${turno.horario} - ${turno.apellido}, ${turno.nombre} (DNI ${turno.dni})`;
-      listaTurnos.appendChild(li);
+        return fechaTurno >= hoy;
     });
-  }
+
+    localStorage.setItem("turnos", JSON.stringify(turnosAgendados));
+}
+limpiarTurnosViejos();
+
+function leerJson() {
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            baseDatos = data;
+            renderizarEspecialidades(data)
+            cargarMedicosEnFiltro();
+            filtrarYMostrar();
+            
+        })
+        .catch(err => console.log("Hubo un error", err))
+        .finally(() => console.log("Finalizo la peticion"))
+}
+leerJson()
+
+
+function renderizarEspecialidades(listaEspecialidades) {
+
+    selectEspecialidad.innerHTML = `<option value="">Seleccione especialidad</option>`;
+
+    listaEspecialidades.forEach(especialidad => {
+        selectEspecialidad.innerHTML += `
+            <option value="${especialidad.id}">
+                ${especialidad.nombre}
+            </option>`;
+    });
 }
 
+selectEspecialidad.addEventListener("change", cargarMedicos);
 
-// FUNCIÓN 5: Agrupar turnos por especialidad
-function agruparTurnosPorEspecialidad(turnos) {
-  const grupos = {};
+function cargarMedicos() {
 
-  turnos.forEach(turno => {
-    if (!grupos[turno.especialidad]) {
-      grupos[turno.especialidad] = [];
+    selectFecha.innerHTML = '<option value="">Seleccione un día</option>';
+    selectHorario.innerHTML = '<option value="">Seleccione un horario</option>';
+
+    const idSeleccionado = parseInt(selectEspecialidad.value);
+
+    const especialidadElegida = baseDatos.find(especialidad => especialidad.id === idSeleccionado);
+    console.log(especialidadElegida)
+    renderizarMedicos(especialidadElegida.medicos);
+}
+
+function renderizarMedicos(listaMedicos) {
+
+    selectMedico.innerHTML = `<option value="">Seleccione médico</option>`;
+
+    listaMedicos.forEach(medico => {
+        selectMedico.innerHTML += `
+            <option value="${medico.id}">
+                ${medico.nombre}
+            </option>
+        `;
+    });
+}
+
+selectMedico.addEventListener("change", CargaFechas);
+
+function CargaFechas() {
+
+        if (selectMedico.value === "") return;
+    const idMedicoSeleccionado = parseInt(selectMedico.value);
+    
+    const idEspSeleccionada = parseInt(selectEspecialidad.value);
+    const especialidadElegida = baseDatos.find(esp => esp.id === idEspSeleccionada);
+    
+    const medicoElegido = especialidadElegida.medicos.find(m => m.id === idMedicoSeleccionado);
+
+    selectFecha.innerHTML = '<option value="">Seleccione un día</option>';
+    selectHorario.innerHTML = '<option value="">Seleccione un horario</option>';
+
+    //const diasNumeros = medicoElegido.dias.map(d => d.dia);
+
+    let hoy = new Date();
+
+for (let i = 0; i < 30; i++) {
+let fecha = new Date(hoy);
+fecha.setDate(fecha.getDate() + i);
+
+    let diaSemana = fecha.getDay();
+
+    for (let j = 0; j < medicoElegido.dias.length; j++) {
+
+        if (medicoElegido.dias[j].dia == diaSemana) {
+
+            let dia = fecha.getDate();
+            let mes = fecha.getMonth() + 1;
+            let anio = fecha.getFullYear();
+
+            let fechaTexto = dia + "/" + mes + "/" + anio;
+
+let fechaISO = fecha.toISOString().split("T")[0];
+
+selectFecha.innerHTML += `
+    <option value="${fechaISO}">
+        ${fechaTexto}
+    </option>
+`;
+        }
     }
-
-    grupos[turno.especialidad].push(turno);
-  });
-
-  return grupos;
 }
 
+}
+
+selectFecha.addEventListener("change", cargarHorarios);
+
+function cargarHorarios() {
+
+    const idEsp = parseInt(selectEspecialidad.value);
+    const idMed = parseInt(selectMedico.value);
+
+    const partes = selectFecha.value.split("-");
+    const fechaSeleccionada = new Date(partes[0], partes[1]-1, partes[2]);
+
+    const diaSemana = fechaSeleccionada.getDay();
+
+    const especialidad = baseDatos.find(e => e.id === idEsp);
+    const medico = especialidad.medicos.find(m => m.id === idMed);
+
+    const diaAtencion = medico.dias.find(d => d.dia === diaSemana);
+
+    if (!diaAtencion) return;
+
+    generarHoras(diaAtencion);
+}
+
+function horaAMinutos(horaTexto){
+    let partes = horaTexto.split(":");
+    let horas = parseInt(partes[0]);
+    let minutos = parseInt(partes[1]);
+
+    return horas*60 + minutos;
+}
+
+function generarHoras(diaAtencion){
+    const especialidadActual = selectEspecialidad.value
+    const medicoActual = selectMedico.value;
+    const fechaActual = selectFecha.value;
+
+    selectHorario.innerHTML = '<option value="">Seleccione horario</option>';
+
+    let desde = horaAMinutos(diaAtencion.desde);
+    let hasta = horaAMinutos(diaAtencion.hasta);
+    let intervalo = diaAtencion.intervalo;
+    
+    // fecha seleccionada convertida a Date
+const partes = selectFecha.value.split("-");
+const fechaSeleccionada = new Date(partes[0], partes[1]-1, partes[2]);
+
+    const hoy = new Date();
+
+    const esHoy =
+        fechaSeleccionada.getDate() === hoy.getDate() &&
+        fechaSeleccionada.getMonth() === hoy.getMonth() &&
+        fechaSeleccionada.getFullYear() === hoy.getFullYear();
+
+    const horaActual = hoy.getHours() * 60 + hoy.getMinutes();
+
+    for(let minutos = desde; minutos < hasta; minutos += intervalo){
+
+        let hora = Math.floor(minutos / 60);
+        let min = minutos % 60;
+
+        let horaTexto = String(hora).padStart(2,"0") + ":" + String(min).padStart(2,"0");
+
+        const ocupado = turnosAgendados.some(turno =>
+        turno.especialidadId == especialidadActual &&
+        turno.medicoId == medicoActual &&
+        turno.fecha == fechaActual &&
+        turno.hora == horaTexto &&
+        turno.estado !== "Cancelado"
+);
+if ( esHoy && !ocupado && minutos>horaActual){
+    selectHorario.innerHTML += `
+        <option value="${horaTexto}">
+            ${horaTexto}
+        </option>
+    `;
+} else if (!esHoy && !ocupado) {
+    selectHorario.innerHTML += `
+        <option value="${horaTexto}">
+            ${horaTexto}
+        </option>
+    `;
+}
+
+    }
+}
+
+formTurno.addEventListener("submit", agendarTurno)
+
+function agendarTurno(e) {
+
+    e.preventDefault();
+
+    // valores paciente
+    const nombre = inputNombre.value;
+    const apellido = inputApellido.value;
+    const dni = inputDni.value;
+    const email = inputEmail.value
+    const telefono = inputTelefono.value
+    const fechaNacimiento = inputFechaNacimiento.value
+
+    // valores de los selects
+    const especialidad = selectEspecialidad.options[selectEspecialidad.selectedIndex].text;
+    const especialidadId = selectEspecialidad.value
+    const medico = selectMedico.options[selectMedico.selectedIndex].text;
+    const medicoId = selectMedico.value
+    const fecha = selectFecha.value;
+    const hora = selectHorario.value;
+
+    const turnoRepetido = turnosAgendados.some(turno => 
+    turno.especialidadId == especialidadId &&
+    turno.medicoId == medicoId &&
+    turno.fecha == fecha &&
+    turno.hora == hora &&
+    turno.estado !="Cancelado"
+);
+
+if (turnoRepetido) {
+    Swal.fire({
+        icon: "error",
+        title: "Horario no disponible",
+        text: "Ya existe un turno asignado en esa fecha y hora"
+    });
+    return;
+}
+
+Swal.fire({
+  title: "¿Confirmar turno?",
+  text: "Revisá que los datos sean correctos",
+  icon: "question",
+  showCancelButton: true,
+  confirmButtonColor: "#0b9128",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Sí, agendar",
+  cancelButtonText: "Cancelar"
+}).then((result) => {
+
+  if (result.isConfirmed) {
+
+           const turnoNuevo = new Turno(
+                nombre,
+                apellido,
+                dni,
+                email,
+                telefono,
+                fechaNacimiento,
+                especialidadId,
+                especialidad,
+                medicoId,
+                medico,
+                fecha,
+                hora
+            );
+
+            turnosAgendados.push(turnoNuevo);
+
+            console.log(turnosAgendados)
+
+            localStorage.setItem("turnos", JSON.stringify(turnosAgendados));
+
+            formTurno.reset();
+
+            renderizarTurnos(turnosAgendados);
+            filtrarYMostrar();
+
+            
+    Swal.fire({
+      title: "Turno Confirmado",
+      text: "Tu turno fue agendado con éxito",
+      icon: "success"
+    } 
+   
+);
+
+
+  }
+});
+
+}
 
---
+++ js/turnos.js
@@ -0,0 +1,197 @@
+const filtroFecha = document.getElementById("filtroFecha");
+const filtroMedico = document.getElementById("filtroMedico");   
+const buscarDni = document.getElementById("buscarDni")
+
+cargarMedicosEnFiltro();
+function cargarMedicosEnFiltro() {
+
+    const filtroMedico = document.getElementById("filtroMedico");
+
+    filtroMedico.innerHTML = '<option value="todos">Todos los médicos</option>';
+
+    baseDatos.forEach(especialidad => {
+        
+        especialidad.medicos.forEach(medico => {
+            
+            filtroMedico.innerHTML += `
+                <option value="${medico.id}">
+                    ${medico.nombre} (${especialidad.nombre})
+                </option>`;
+        });
+    });
+}
+
+const listaTurnos = document.getElementById("listaTurnos");
+
+filtroMedico.addEventListener("change", filtrarYMostrar);
+filtroFecha.addEventListener("change", filtrarYMostrar);
+buscarDni.addEventListener("input", filtrarYMostrar);
+
+function filtrarYMostrar() {
+
+    let turnosFiltrados = [...turnosAgendados];
+
+    const idMedicoSeleccionado = filtroMedico.value;
+    const fechaElegida = filtroFecha.value;
+    const dniSeleccionado = buscarDni.value.trim();
+
+    if (idMedicoSeleccionado !== "todos") {
+        turnosFiltrados = turnosFiltrados.filter(t => 
+            t.medicoId == idMedicoSeleccionado
+        );
+    }
+
+    if (fechaElegida !== "") {
+
+
+        turnosFiltrados = turnosFiltrados.filter(t => 
+            t.fecha === fechaElegida
+        );
+    }
+
+if (dniSeleccionado !== "") {
+    turnosFiltrados = turnosFiltrados.filter(t => 
+        String(t.dni).includes(dniSeleccionado)
+    );
+}
+turnosFiltrados.sort((a, b) => {
+
+    const fechaHoraA = new Date(`${a.fecha}T${a.hora}`);
+    const fechaHoraB = new Date(`${b.fecha}T${b.hora}`);
+
+    return fechaHoraA - fechaHoraB;
+});
+    renderizarTurnos(turnosFiltrados);
+}
+
+function renderizarTurnos(lista){
+
+    listaTurnos.innerHTML = "";
+
+    lista.forEach(turno => {
+
+        let colorEstado = "secondary";
+
+        if(turno.estado === "Atendido") {
+            colorEstado = "success"
+            listaTurnos.innerHTML += `
+        <li class="list-group-item">
+            <div class="d-flex justify-content-between align-items-center">
+
+                <div>
+                    <b>${turno.hora} | ${turno.fecha}</b><br>
+                    ${turno.nombre} ${turno.apellido} | 
+                    DNI: ${turno.dni} <br>
+                    ${turno.especialidad} | ${turno.medico} <br>
+                    <span class="badge bg-${colorEstado} ms-2">${turno.estado}</span>
+                </div>
+        `;
+        }
+
+           if(turno.estado === "Cancelado") {
+            colorEstado = "danger"
+            listaTurnos.innerHTML += `
+        <li class="list-group-item">
+            <div class="d-flex justify-content-between align-items-center">
+
+                <div>
+                    <b>${turno.hora} | ${turno.fecha}</b><br>
+                    ${turno.nombre} ${turno.apellido} | 
+                    DNI: ${turno.dni} <br>
+                    ${turno.especialidad} | ${turno.medico} <br>
+                    <span class="badge bg-${colorEstado} ms-2">${turno.estado}</span>
+                </div>
+        `;
+
+           }
+        if(turno.estado === "Pendiente") {
+        listaTurnos.innerHTML += `
+        <li class="list-group-item">
+            <div class="d-flex justify-content-between align-items-center">
+
+                <div>
+                    <b>${turno.hora} | ${turno.fecha}</b><br>
+                    ${turno.nombre} ${turno.apellido} | 
+                    DNI: ${turno.dni} <br>
+                    ${turno.especialidad} | ${turno.medico} <br>
+                    <span class="badge bg-${colorEstado} ms-2">${turno.estado}</span>
+                </div>
+
+                <div class="d-flex flex-row flex-nowrap gap-1">
+                    <button class="btn btn-success btn-sm me-2" onclick="atenderTurno(${turno.id})">
+                        Atender
+                    </button>
+
+                    <button class="btn btn-danger btn-sm" onclick="cancelarTurno(${turno.id})">
+                        Cancelar
+                    </button>
+                </div>
+
+            </div>
+        </li>
+        `;}
+    });
+}
+
+renderizarTurnos(turnosAgendados);
+
+function atenderTurno(id) {
+
+Swal.fire({
+  title: "¿Desea atender este turno?",
+  text: "El turno cambiará de estado a Atendido",
+  icon: "question",
+  showCancelButton: true,
+  confirmButtonColor: "#076439",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Sí, atender",
+  cancelButtonText: "Cancelar"
+}).then((result) => {
+  if (result.isConfirmed) {
+turnosAgendados = turnosAgendados.map(t => {
+        if (t.id === id) t.estado = "Atendido";
+        return t;
+    });
+    localStorage.setItem("turnos", JSON.stringify(turnosAgendados));
+    filtrarYMostrar();
+
+    Swal.fire({
+      title: "Turno actualizado",
+      text: "El estado fue modificado correctamente",
+      icon: "success"
+    });
+  }
+});    
+}
+
+function cancelarTurno(id) {
+
+Swal.fire({
+  title: "¿Desea cancelar este turno?",
+  text: "El turno cambiará de estado a Cancelado",
+  icon: "question",
+  showCancelButton: true,
+  confirmButtonColor: "#d33",
+  cancelButtonColor: "rgb(51, 91, 221)",
+  confirmButtonText: "Sí, cancelar",
+  cancelButtonText: "Salir"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+    turnosAgendados = turnosAgendados.map(t => {
+        if (t.id === id) t.estado = "Cancelado";
+        return t;
+    });
+    localStorage.setItem("turnos", JSON.stringify(turnosAgendados));
+    filtrarYMostrar();
+
+
+    Swal.fire({
+      title: "Turno actualizado",
+      text: "El estado fue modificado correctamente",
+      icon: "success"
+    });
+  }
+});
+
+}
